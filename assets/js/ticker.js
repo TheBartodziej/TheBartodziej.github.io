95a4e1c1883360dcfefc5b64dbb8dc454c144621
rychu-tech <rychudotka14@gmail.com>
2025-12-02 18:32:17 +0100

    Change uni section

--- assets/js/ticker.js
+++ assets/js/ticker.js
@@ -1,17 +1,38 @@
-const track = document.querySelector(".ticker-track");
+    const track = document.querySelector(".ticker-track");
 
-const children = Array.from(track.children);
-children.forEach((child) => {
-  track.appendChild(child.cloneNode(true));
+    const children = Array.from(track.children);
+    children.forEach((child) => {
+    track.appendChild(child.cloneNode(true));
 });
 
-let x = 0;
+    let x = 0;
+    let animating = false;
+    let started = false;
 
-function animate() {
-  x -= 1;
-  if (x <= -track.scrollWidth / 2) x = 0;
-  track.style.transform = `translateX(${x}px)`;
-  requestAnimationFrame(animate);
+    function animate() {
+    if (!animating) return;
+
+    x -= 1;
+    if (x <= -track.scrollWidth / 2) x = 0;
+    track.style.transform = `translateX(${x}px)`;
+
+    requestAnimationFrame(animate);
 }
 
-animate();
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting && !started) {
+                    started = true;
+
+                    setTimeout(() => {
+                        animating = true;
+                        animate();
+                    }, 500);
+                }
+            });
+        },
+        { threshold: 0.2 }
+    );
+
+    observer.observe(track);
